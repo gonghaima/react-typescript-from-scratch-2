672474c91db4647828dcb352f46ab99cb0942d9d
Steven <gonghaima@hotmail.com>
2022-09-26 22:05:42 +1000

    multiple environments

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import LOGO from './logo.svg';
 export const App = () => {
   return (
     <>
-      <h1>React Typescript Webpack Starter Template</h1>;
+      <h1>
+        React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      ;
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={LOGO} alt="React Logo" width="300" height="200" />
     </>
