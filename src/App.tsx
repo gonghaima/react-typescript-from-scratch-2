509a0845d49b19497381f9c1cae885f55e9d34a2
Steven <gonghaima@hotmail.com>
2022-09-27 22:16:25 +1000

    hot reload without page refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import './styles.css';
 import IMAGE from './react.png';
 import LOGO from './logo.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
   return (
     <>
       <h1>
-        React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
+         React Typescript Webpack Starter Template - {process.env.NODE_ENV}{' '}
+        {process.env.name}
       </h1>
       ;
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={LOGO} alt="React Logo" width="300" height="200" />
+      <ClickCounter />
     </>
   );
 };
